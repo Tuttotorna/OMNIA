a0eff1759a1d2b690d6c028b29a1ee523f33a016
Massimiliano Brighindi brighissimo@gmail.com <brighissimo@gmail.com>
2026-04-19 16:21:38 +0200

    Update __init__.py

--- omnia/__init__.py
+++ omnia/__init__.py
@@ -1,3 +1,8 @@
 from omnia.core import run_omnia
 
-__all__ = ["run_omnia"]
+
+def evaluate_structural_profile(case):
+    return run_omnia(case)
+
+
+__all__ = ["run_omnia", "evaluate_structural_profile"]
